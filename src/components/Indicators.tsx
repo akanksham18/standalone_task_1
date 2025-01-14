fa7370e93a59128fb6bd0e4c736f6b500d9bcdd7
akanksha18 <akanksha181@gmail.com>
2025-01-14 12:56:27 +0530

    mergin with akanksha

--- src/components/Indicators.tsx
+++ src/components/Indicators.tsx
@@ -1,13 +1,8 @@
-<<<<<<<< < Temporary merge branch 1
-import Indicator from "./Indicator";
-=========
-import { Indicator } from './index'
->>>>>>>>> Temporary merge branch 2
+import { Indicator } from './Indicator'
 
 const Indicators = () => {
     const texts = ["OEE", "Production", "Idle", "Lock/Unlock"];
 
-<<<<<<<<< Temporary merge branch 1
     const containerStyles = [
         "left-[26px]",
         "left-[198px]",
@@ -31,21 +26,15 @@ const Indicators = () => {
                     customStyle={containerStyles[index]}
                     spanStyle={spanStyles[index]}
                 />
-=========
     const style= "h-24 w-48 flex items-center justify-center px-10";
 
-    return (
-        <div className="flex gap-8 justify-between">
-            {texts.map((text, index) => (
-                <Indicator key={index} text={text} style={style} />
->>>>>>>>> Temporary merge branch 2
-            ))}
-        </div>
-    );
-};
+            return (
+            <div className="flex gap-8 justify-between">
+                {texts.map((text, index) => (
+                    <Indicator key={index} text={text} />
+                ))}
+            </div>
+            );
+        };
+            export default Indicators;
 
-<<<<<<<<< Temporary merge branch 1
-export default Indicators;
-=========
-export default Indicators;
->>>>>>>>> Temporary merge branch 2
